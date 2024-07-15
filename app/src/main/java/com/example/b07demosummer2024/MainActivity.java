6f7ba47d5da6052e15b062ee495d9b18cea332e1
HBenipal <115516035+HBenipal@users.noreply.github.com>
2024-07-15 10:21:48 -0400

    Add firebase links

--- app/src/main/java/com/example/b07demosummer2024/MainActivity.java
+++ app/src/main/java/com/example/b07demosummer2024/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db = FirebaseDatabase.getInstance("https://b07-demo-summer-2024-default-rtdb.firebaseio.com/");
+        db = FirebaseDatabase.getInstance("https://cscb07final-default-rtdb.firebaseio.com/");
         DatabaseReference myRef = db.getReference("testDemo");
 
 //        myRef.setValue("B07 Demo!");
