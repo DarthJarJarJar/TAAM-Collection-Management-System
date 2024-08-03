76e32216f7d8c1bef4d533d605788bc9eb5dd552
HarguntasBenipal <harguntasbenipal16@gmail.com>
2024-08-03 11:56:12 -0400

    fix: display list of title and lot numbers for each chosen item to delete.

--- app/src/main/java/com/example/b07demosummer2024/DeleteItemFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/DeleteItemFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,20 +24,22 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DeleteItemFragment extends Fragment {
-    private TextView confirmTitle;
-    private ListView delete_list;
-    private Button buttonDelete;
 
-    private List<Pair<String, Integer>> delete_sublist = new ArrayList<>();
+
+    private Button buttonDelete;
+    private ListView delete_list;
+
+    private List<Map<String, String>> output_delete_list = new ArrayList<Map<String, String>>();
 
     private FirebaseDatabase db;
     private DatabaseReference itemsRef;
 
     private List<Item> itemList;
-
 
     public static DeleteItemFragment newInstance(List<Item> itemList) {
         DeleteItemFragment fragment = new DeleteItemFragment();
@@ -49,28 +52,23 @@ public class DeleteItemFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_delete_item, container, false);
 
-        confirmTitle = view.findViewById(R.id.confirm_Title);
-        delete_list = view.findViewById(R.id.list);
-        //spinnerCategory = view.findViewById(R.id.spinnerCategory);
+        delete_list = view.findViewById(R.id.delete_list);
         buttonDelete = view.findViewById(R.id.buttonDelete);
 
         db = FirebaseDatabase.getInstance("https://b07-demo-summer-2024-default-rtdb.firebaseio.com/");
 
-
         // Set up the spinner with categories
         for(int i = 0; i < itemList.size(); i++){
             Item curItem = itemList.get(i);
-            delete_sublist.add(new Pair<>(curItem.getTitle(), curItem.getId()));
+            Map<String, String> data = new HashMap<String, String>(2);
+            data.put("Title", curItem.getTitle());
+            data.put("Lot Number", Integer.toString(curItem.getId()));
+            output_delete_list.add(data);
         }
 
+        SimpleAdapter adapter = new SimpleAdapter(getContext(), output_delete_list, android.R.layout.simple_list_item_2, new String[] {"Title", "Lot Number"}, new int[] {android.R.id.text1, android.R.id.text2});
+        delete_list.setAdapter(adapter);
 
-
-        ArrayAdapter<Pair<String, Integer>> arr = new ArrayAdapter<Pair<String, Integer>>(getContext(), R.layout.chosen_to_delete_items, R.id.test, delete_sublist);
-        delete_list.setAdapter(arr);
-        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
-        //   R.array.categories_array, android.R.layout.simple_spinner_item);
-        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //spinnerCategory.setAdapter(adapter);
 
         buttonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
