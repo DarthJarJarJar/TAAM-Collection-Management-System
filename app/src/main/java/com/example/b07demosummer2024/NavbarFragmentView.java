6bc1717ff354f6d0551ca6cca64006aa71f45b23
Eric Liu <80423441+eliu0412@users.noreply.github.com>
2024-08-06 00:43:19 -0400

    fix: added import

--- app/src/main/java/com/example/b07demosummer2024/NavbarFragmentView.java
+++ app/src/main/java/com/example/b07demosummer2024/NavbarFragmentView.java
@@ -12,6 +12,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import java.util.ArrayList;
+
 import java.util.List;
 
 public class NavbarFragmentView extends Fragment {
