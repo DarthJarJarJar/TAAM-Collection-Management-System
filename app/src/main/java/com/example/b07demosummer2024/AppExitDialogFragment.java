f063fead1adfa7fe5b95f6fff5bb56bb78238a6f
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2024-08-07 11:38:33 -0400

    fix: retain login state on app exit, confirm exit

--- app/src/main/java/com/example/b07demosummer2024/AppExitDialogFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/AppExitDialogFragment.java
@@ -1,2 +1,31 @@
-package com.example.b07demosummer2024;public class AppExitDialogFragment {
+package com.example.b07demosummer2024;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class AppExitDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.exit_question)
+                .setPositiveButton(R.string.exit, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (getActivity() != null) {
+                            getActivity().finish();
+                            System.exit(0);
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // cancel
+                    }
+                });
+        return builder.create();
+    }
 }
