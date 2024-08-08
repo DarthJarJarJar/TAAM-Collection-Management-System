30f1c3075b0f536893946759f6c190cf602e31a9
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2024-08-07 22:45:29 -0400

    fix: update documentaion and formatted delete

--- app/src/main/java/com/example/b07demosummer2024/DeleteItemFragmentPresenter.java
+++ app/src/main/java/com/example/b07demosummer2024/DeleteItemFragmentPresenter.java
@@ -14,64 +14,87 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * the presenter for DeleteItem fragment
+ * presenter for the delete item fragment
  */
 public class DeleteItemFragmentPresenter implements DeleteItemFragmentPresenterInterface {
 
-    DeleteItemFragmentModel model;
-    DeleteItemFragmentView view;
-    private SearchFragmentModel search;
-    Context context;
+  DeleteItemFragmentModel model;
+  DeleteItemFragmentView view;
+  private SearchFragmentModel search;
+  Context context;
 
 
-    int counter = 1;
-    private List<Map<String, String>> output_delete_list = new ArrayList<Map<String, String>>();
-    private List<Item> itemList;
+  int counter = 1;
+  private List<Map<String, String>> output_delete_list = new ArrayList<Map<String, String>>();
+  private List<Item> itemList;
 
-    public DeleteItemFragmentPresenter(Context context, DeleteItemFragmentView view, DeleteItemFragmentModel model) {
-        model.presenterInterface = this;
-        this.view = view;
-        this.model = model;
-        this.context = context;
-    }
+  /**
+   * constructor for the presenter
+   *
+   * @param context the context
+   * @param view    the view
+   * @param model   the model
+   */
+  public DeleteItemFragmentPresenter(Context context, DeleteItemFragmentView view,
+      DeleteItemFragmentModel model) {
+    model.presenterInterface = this;
+    this.view = view;
+    this.model = model;
+    this.context = context;
+  }
 
-    void iterate_delete_items() {
-        for (Item curItem : itemList) {
-            model.remove_item(curItem, new DeletionSuccessListener() {
-                @Override
-                public void onSuccess(String category, String period) {
-                    checkRemoveCategory(category);
-                    checkRemovePeriod(period);
-                }
-            });
+  /**
+   * iterates over itemList and attempts to delete all the items in it, and all the non-default
+   * categories and periods if there are no items corresponding to them left in the DB
+   */
+  void iterate_delete_items() {
+    for (Item curItem : itemList) {
+      model.remove_item(curItem, new DeletionSuccessListener() {
+        @Override
+        public void onSuccess(String category, String period) {
+          checkRemoveCategory(category);
+          checkRemovePeriod(period);
         }
       });
-    }
-
-
-    private void checkRemoveCategory(String category){
-        List<Item> res = search.filterItems(-1, "", category, "", true, false, "");
-        String[] defaultCategories = context.getResources().getStringArray(R.array.categories_array);
-        List<String> defaultCategoriesList = Arrays.asList(defaultCategories);
-
-        if(res.isEmpty() && !defaultCategoriesList.contains(category)){
-            model.removeField("Categories", category);
-        }
-    }
-
-    private void checkRemovePeriod(String period){
-        List<Item> res = search.filterItems(-1, "", "", period, false, true, "");
-        String[] defaultPeriods = context.getResources().getStringArray(R.array.period_array);
-        List<String> defaultPeriodsList = Arrays.asList(defaultPeriods);
-
-        if(res.isEmpty() && !defaultPeriodsList.contains(period)){
-            model.removeField("Periods", period);
-        }
     }
   }
 
   /**
-   * closes the delete screen and displays a success message once all items are deleted
+   * checks if a category has any more items associated with it in the db. if not, removes that
+   * category from the db if it is a non-default one
+   *
+   * @param category the category to check
+   */
+  private void checkRemoveCategory(String category) {
+    List<Item> res = search.filterItems(-1, "", category, "",
+        true, false, "");
+    String[] defaultCategories = context.getResources().getStringArray(R.array.categories_array);
+    List<String> defaultCategoriesList = Arrays.asList(defaultCategories);
+
+    if (res.isEmpty() && !defaultCategoriesList.contains(category)) {
+      model.removeField("Categories", category);
+    }
+  }
+
+  /**
+   * checks if a period has any more items associated with it in the db. if not, removes that period
+   * from the db if it is a non-default one
+   *
+   * @param period the category to check
+   */
+  private void checkRemovePeriod(String period) {
+    List<Item> res = search.filterItems(-1, "", "", period,
+        false, true, "");
+    String[] defaultPeriods = context.getResources().getStringArray(R.array.period_array);
+    List<String> defaultPeriodsList = Arrays.asList(defaultPeriods);
+
+    if (res.isEmpty() && !defaultPeriodsList.contains(period)) {
+      model.removeField("Periods", period);
+    }
+  }
+
+  /**
+   * closes the delete screen and displays the number of items deleted as a toast message
    */
   @Override
   public void close() {
@@ -84,9 +107,9 @@ public class DeleteItemFragmentPresenter implements DeleteItemFragmentPresenterI
   }
 
   /**
-   * shows failure message as a toast
+   * shows a toast with a failure message
    *
-   * @param message the message
+   * @param message the message to be shown
    */
   @Override
   public void printFailure(String message) {
@@ -94,27 +117,27 @@ public class DeleteItemFragmentPresenter implements DeleteItemFragmentPresenterI
   }
 
   /**
-   * sets the search fragment model
+   * sets the search model
    *
-   * @param search the search fragment model
+   * @param search the search model
    */
   void setSearch(SearchFragmentModel search) {
     this.search = search;
   }
 
   /**
-   * initializes the itemList
+   * sets the itemList field by copying all elements of the given itemList to it
    *
-   * @param itemList the list of items
+   * @param itemList the list of items to copy from
    */
   void setItemList(List<Item> itemList) {
     this.itemList = new ArrayList<>(itemList);
   }
 
   /**
-   * sets up the spinner and adds items to the output delete list to be returned
+   * sets up the delete items preview list with all the items to be deleted
    *
-   * @return the list of items that are going to be deleted
+   * @return list of title-lot number maps of the selected items to be deleted
    */
   List<Map<String, String>> setupSpinner() {
     for (int i = 0; i < itemList.size(); i++) {
@@ -126,6 +149,4 @@ public class DeleteItemFragmentPresenter implements DeleteItemFragmentPresenterI
     }
     return output_delete_list;
   }
-
-
 }
